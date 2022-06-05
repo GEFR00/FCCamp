5e80422d131831b2899aaa88d4062cb4a3eca150
Gabriela Figueroa R <elizabehtriquelme@gmail.com>
2022-06-05 19:04:11 -0400

    ejercicio resuelto

--- ejRecordCollection.js
+++ ejRecordCollection.js
@@ -33,6 +33,19 @@ const recordCollection = {
   
   // Only change code below this line
   function updateRecords(records, id, prop, value) {
+      //Condiciones entregadas
+      if (prop !== "tracks" && value !== "") {
+        records[id][prop] = value;
+      } 
+      else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+        records[id][prop] = [value];
+      } 
+      else if (prop === "tracks" && value !== "") {
+        records[id][prop].push(value);
+      } 
+      else if (value === "") {
+        delete records[id][prop];
+      }
     return records;
   }
   
